738bbc5dc342d1544e79464f9747cbaff5fbf2c7
chaseleinart <chaseleinart@gmail.com>
2017-10-26 19:48:23 -0400

    include firebase initial code

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,5 +1,70 @@
-$("#submitButton").on("click", function(event) {
-console.log("this is successful.")
-alert("test")
-event.preventDefault();
+// $("#submitButton").on("click", function(event) {
+// console.log("this is successful.")
+// alert("test")
+// event.preventDefault();
+// });
+
+
+var config = {
+    apiKey: "AIzaSyBllCLHjQIhELVmqbxECRjbd7QWdgJI3qE",
+    authDomain: "test-project-f55ba.firebaseapp.com",
+    databaseURL: "https://test-project-f55ba.firebaseio.com",
+    projectId: "test-project-f55ba",
+    storageBucket: "test-project-f55ba.appspot.com",
+    messagingSenderId: "476902605350"
+  };
+  firebase.initializeApp(config);
+
+// Create a variable to reference the database.
+var database = firebase.database();
+
+// Initial Values
+var name = "";
+var role = "";
+var startDate = "";
+var monthlyRate = "";
+
+// Capture Button Click
+$("#submitButton").on("click", function (event) {
+    event.preventDefault();
+
+    // Grabbed values from text-boxes
+    name = $("#employeename").val().trim();
+    console.log(name);
+    role = $("#role").val().trim();
+    console.log(role);
+    startDate = $("#startDate").val().trim();
+    console.log(startDate);
+    monthlyRate = $("#monthlyRate").val().trim();
+    console.log(monthlyRate);
+
+    // Code for "Setting values in the database"
+    database.ref().push({
+        name: name,
+        role: role,
+        startDate: startDate,
+        monthlyRate: monthlyRate
+    });
+
 });
+
+// // Firebase watcher + initial loader HINT: .on("value")
+// database.ref().on("value", function (snapshot) {
+
+//     // Log everything that's coming out of snapshot
+//     console.log(snapshot.val());
+//     console.log(snapshot.val().name);
+//     console.log(snapshot.val().email);
+//     console.log(snapshot.val().age);
+//     console.log(snapshot.val().comment);
+
+//     // Change the HTML to reflect
+//     $("#name-display").text(snapshot.val().name);
+//     $("#email-display").text(snapshot.val().email);
+//     $("#age-display").text(snapshot.val().age);
+//     $("#comment-display").text(snapshot.val().comment);
+
+//     // Handle the errors
+// }, function (errorObject) {
+//     console.log("Errors handled: " + errorObject.code);
+// });
