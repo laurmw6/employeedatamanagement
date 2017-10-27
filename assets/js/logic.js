150403735583d5b12bb897115c24f0e444e3dabc
chaseleinart <chaseleinart@gmail.com>
2017-10-26 21:29:46 -0400

    clean up js

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -12,8 +12,8 @@ var config = {
     projectId: "test-project-f55ba",
     storageBucket: "test-project-f55ba.appspot.com",
     messagingSenderId: "476902605350"
-  };
-  firebase.initializeApp(config);
+};
+firebase.initializeApp(config);
 
 // Create a variable to reference the database.
 var database = firebase.database();
@@ -23,6 +23,8 @@ var name = "";
 var role = "";
 var startDate = "";
 var monthlyRate = "";
+var monthsWorked = "";
+var totalBilled = "";
 
 // Capture Button Click
 $("#submitButton").on("click", function (event) {
@@ -38,33 +40,47 @@ $("#submitButton").on("click", function (event) {
     monthlyRate = $("#monthlyRate").val().trim();
     console.log(monthlyRate);
 
+
+    monthsWorked = -(moment(startDate).diff(moment(), "months"));
+    console.log("months worked" + monthsWorked);
+    totalBilled = (monthsWorked * monthlyRate);
+    console.log("total billed: " + totalBilled);
+
     // Code for "Setting values in the database"
     database.ref().push({
         name: name,
         role: role,
         startDate: startDate,
-        monthlyRate: monthlyRate
+        monthlyRate: monthlyRate,
+        monthsWorked: monthsWorked,
+        totalBilled: totalBilled
     });
 
 });
 
 
-database.ref().on("child_added", function(childSnapshot) {
+database.ref().on("child_added", function (childSnapshot) {
     // Log everything that's coming out of snapshot
     console.log(childSnapshot.val().name);
     console.log(childSnapshot.val().role);
     console.log(childSnapshot.val().startDate);
     console.log(childSnapshot.val().monthlyRate);
 
+
+
+
+
     // full list of items to the well
     $("#current-employees").append("<div class='well'><span id='name'> " + childSnapshot.val().name +
         " </span><span id='email'> " + childSnapshot.val().role +
         " </span><span id='age'> " + childSnapshot.val().startDate +
-        " </span><span id='comment'> " + childSnapshot.val().monthlyRate + " </span></div>");
+        " </span><span id='comment'> " + childSnapshot.val().monthlyRate +
+        " </span><span id='comment'> " + childSnapshot.val().monthsWorked + 
+        " </span><span id='comment'> " + childSnapshot.val().totalBilled + " </span></div>");
 
-        
+
     // Handle the errors
-}, function(errorObject) {
+}, function (errorObject) {
     console.log("Errors handled: " + errorObject.code);
 });
 
